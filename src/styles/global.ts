fce0a6b77355f7a2af75f75d9a550f4d7ae34c29
JAMDev <jamdeveloper0@gmail.com>
2022-10-26 18:58:54 -0300

    Modificações nos estilos globais

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,15 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyles = createGlobalStyle`
   * {
     box-sizing: border-box;
+
+    margin: 0;
+    padding: 0;
+
+    :focus {
+      outline: none;
+
+      box-shadow: 0px 0px 0px 2px ${(params) => params.theme.colors.brand.blue};
+    }
   }
 
   body {
@@ -12,6 +21,8 @@ export const GlobalStyles = createGlobalStyle`
   body, button, input, p, a {
     font-size: 1rem;
     font-weight: normal;
-    color: ${(params) => params.theme.colors.base.text}
+    color: ${(params) => params.theme.colors.base.text};
+    line-height: 160%;
+    font-family: 'Nunito', sans-serif;
   }
 `
