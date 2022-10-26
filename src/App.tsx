8d108ada65c0e87af2ed541a2128b2a82ff77a02
JAMDev <jamdeveloper0@gmail.com>
2022-10-26 20:30:23 -0300

    Implementação do Provider do useProfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { ProfileProvider } from './hooks/useProfile'
 import { Router } from './Router'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -8,9 +9,11 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <ProfileProvider>
+          <Router />
 
-        <GlobalStyles />
+          <GlobalStyles />
+        </ProfileProvider>
       </BrowserRouter>
     </ThemeProvider>
   )
